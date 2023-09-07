30d4d7005bb5eb9588a6d1786397f5cb339bf4bd
Do-Re-Min-Woo <kimy5878@naver.com>
2023-09-07 16:22:10 +0900

    fix : 바텀 네비게이션 변경

--- TripFriend/app/src/main/java/com/test/tripfriend/MainActivity.kt
+++ TripFriend/app/src/main/java/com/test/tripfriend/MainActivity.kt
@@ -1,17 +1,28 @@
 package com.test.tripfriend
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.test.tripfriend.databinding.ActivityMainBinding
+import com.test.tripfriend.ui.chatting.ChattingMainFragment
+import com.test.tripfriend.ui.home.HomeMainFragment
+import com.test.tripfriend.ui.myinfo.MyInfoMainFragment
+import com.test.tripfriend.ui.trip.TripMainFragment
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var activityMainBinding: ActivityMainBinding
+    var selectMenu = 0
+
+    companion object {
+        val HOME_MAIN_FRAGMENT = "HomeMainFragment"
+        val TRIP_MAIN_FRAGMENT = "TripMainFragment"
+        val CHATTING_MAIN_FRAGMENT = "ChattingMainFragment"
+        val MYINFO_MAIN_FRAGMENT = "MyInfoMainFragment"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,16 +30,90 @@ class MainActivity : AppCompatActivity() {
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
 
-        val navView: BottomNavigationView = activityMainBinding.navView
+        // 시작화면 설정
+        replaceFragment(HOME_MAIN_FRAGMENT, false, true, null)
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        activityMainBinding.run {
+            bottomNavigationViewMain.run {
+                this.selectedItemId = R.id.navigationHome
+                selectMenu = R.id.navigationHome
 
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigationHome, R.id.navigationTrip, R.id.navigationChatting, R.id.navigationMyInfo
-            )
-        )
-        // setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+                setOnItemReselectedListener {
+//                    //선택된 메뉴를 다시 클릭할 때 선택을 넘기는 조건문
+//                    if (it.itemId == selectedItemId){
+//                        return@setOnItemReselectedListener
+//                    }
+                    when (it.itemId) {
+                        //홈 클릭
+                        R.id.navigationHome -> {
+                            selectMenu = it.itemId
+                            replaceFragment(HOME_MAIN_FRAGMENT, false, true, null)
+                        }
+                        //여행 클릭
+                        R.id.navigationTrip -> {
+                            selectMenu = it.itemId
+                            replaceFragment(TRIP_MAIN_FRAGMENT,false,true,null)
+                        }
+                        //채팅 클릭
+                        R.id.navigationChatting -> {
+                            selectMenu = it.itemId
+                            replaceFragment(CHATTING_MAIN_FRAGMENT, false, true, null)
+                        }
+                        //내정보 클릭
+                        R.id.navigationMyInfo -> {
+                            selectMenu = it.itemId
+                            replaceFragment(MYINFO_MAIN_FRAGMENT, false, true, null)
+                        }
+                        else -> {
+                            replaceFragment(HOME_MAIN_FRAGMENT, false, true, null)
+                        }
+                    }
+                    true
+                }
+            }
+        }
+
+    }
+
+    // 지정한 Fragment를 보여주는 메서드
+    fun replaceFragment(name: String, addToBackStack: Boolean, animate: Boolean, bundle: Bundle?) {
+        // Fragment 교체 상태로 설정한다.
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+        // 새로운 Fragment를 담을 변수
+        var newFragment = when (name) {
+            HOME_MAIN_FRAGMENT -> HomeMainFragment()
+            TRIP_MAIN_FRAGMENT -> TripMainFragment()
+            CHATTING_MAIN_FRAGMENT -> ChattingMainFragment()
+            MYINFO_MAIN_FRAGMENT -> MyInfoMainFragment()
+
+            else -> Fragment()
+        }
+
+        newFragment.arguments = bundle
+
+        if (newFragment != null) {
+
+            // Fragment를 교체한다.
+            fragmentTransaction.replace(R.id.fragmentContainerMain, newFragment)
+
+            if (animate) {
+                // 애니메이션을 설정한다.
+                fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            }
+
+            if (addToBackStack) {
+                // Fragment를 Backstack에 넣어 이전으로 돌아가는 기능이 동작할 수 있도록 한다.
+                fragmentTransaction.addToBackStack(name)
+            }
+
+            // 교체 명령이 동작하도록 한다.
+            fragmentTransaction.commit()
+        }
+    }
+
+    // Fragment를 BackStack에서 제거한다.
+    fun removeFragment(name: String) {
+        supportFragmentManager.popBackStack(name, FragmentManager.POP_BACK_STACK_INCLUSIVE)
     }
 }
