cacbae54fde9b27e9d49a554363f8defbb1e1e70
Ganghyungoo <104668071+Ganghyungoo@users.noreply.github.com>
2023-09-11 10:50:06 +0900

    UI: 내 정보 메인 화면 UI구현 및 seekbar 커스텀

--- TripFriend/app/src/main/java/com/test/tripfriend/MainActivity.kt
+++ TripFriend/app/src/main/java/com/test/tripfriend/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.FragmentTransaction
 import com.test.tripfriend.databinding.ActivityMainBinding
 import com.test.tripfriend.ui.chatting.ChattingMainFragment
 import com.test.tripfriend.ui.home.HomeMainFragment
+import com.test.tripfriend.ui.myinfo.MyAccompanyInfoFragment
 import com.test.tripfriend.ui.myinfo.MyInfoMainFragment
 import com.test.tripfriend.ui.trip.TripMainFragment
 
@@ -21,7 +22,8 @@ class MainActivity : AppCompatActivity() {
         val HOME_MAIN_FRAGMENT = "HomeMainFragment"
         val TRIP_MAIN_FRAGMENT = "TripMainFragment"
         val CHATTING_MAIN_FRAGMENT = "ChattingMainFragment"
-        val MYINFO_MAIN_FRAGMENT = "MyInfoMainFragment"
+        val MY_INFO_MAIN_FRAGMENT = "MyInfoMainFragment"
+        val MY_ACCOMPANY_INFO_FRAGMENT="MyAccompanyInfoFragment"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -62,7 +64,7 @@ class MainActivity : AppCompatActivity() {
                         //내정보 클릭
                         R.id.navigationMyInfo -> {
                             selectMenu = it.itemId
-                            replaceFragment(MYINFO_MAIN_FRAGMENT, false, true, null)
+                            replaceFragment(MY_INFO_MAIN_FRAGMENT, false, true, null)
                         }
                         else -> {
                             replaceFragment(HOME_MAIN_FRAGMENT, false, true, null)
@@ -85,7 +87,8 @@ class MainActivity : AppCompatActivity() {
             HOME_MAIN_FRAGMENT -> HomeMainFragment()
             TRIP_MAIN_FRAGMENT -> TripMainFragment()
             CHATTING_MAIN_FRAGMENT -> ChattingMainFragment()
-            MYINFO_MAIN_FRAGMENT -> MyInfoMainFragment()
+            MY_INFO_MAIN_FRAGMENT -> MyInfoMainFragment()
+            MY_ACCOMPANY_INFO_FRAGMENT->MyAccompanyInfoFragment()
 
             else -> Fragment()
         }
