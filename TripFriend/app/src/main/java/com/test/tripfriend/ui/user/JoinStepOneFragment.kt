0aa99b191d0449e1abccb1088fa2bab40a7d3749
nueijeel <lge6716@gmail.com>
2023-09-12 23:05:33 +0900

    UI : 회원가입 1단계 화면에 이용 약관, 개인정보 동의 약관 구현

--- TripFriend/app/src/main/java/com/test/tripfriend/ui/user/JoinStepOneFragment.kt
+++ TripFriend/app/src/main/java/com/test/tripfriend/ui/user/JoinStepOneFragment.kt
@@ -1,5 +1,6 @@
 package com.test.tripfriend.ui.user
 
+import android.content.DialogInterface
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.test.tripfriend.R
 import com.test.tripfriend.databinding.FragmentJoinStepOneBinding
 
@@ -41,6 +43,26 @@ class JoinStepOneFragment : Fragment() {
             //버튼 클릭 화면 전환
             buttonJoinStepOneNext.setOnClickListener {
                 loginMainActivity.replaceFragment(LoginMainActivity.JOIN_STEP_TWO_FRAGMENT, true, true, null)
+            }
+
+            //이용 약관 내용 보기
+            textViewJoinStepOneServiceTerms.setOnClickListener {
+                val builder= MaterialAlertDialogBuilder(loginMainActivity,R.style.DialogTheme).apply {
+                    setTitle("Trip Friend 서비스 이용 약관")
+                    setMessage(R.string.service_terms_contents)
+                    setNegativeButton("확인", null)
+                }
+                builder.show()
+            }
+
+            //개인 정보 약관 내용 보기
+            textViewJoinStepOnePersonalInfoTerms.setOnClickListener {
+                val builder= MaterialAlertDialogBuilder(loginMainActivity,R.style.DialogTheme).apply {
+                    setTitle("개인 정보 수집 및 이용 동의")
+                    setMessage(R.string.personal_info_terms_contents)
+                    setNegativeButton("확인", null)
+                }
+                builder.show()
             }
         }
 
