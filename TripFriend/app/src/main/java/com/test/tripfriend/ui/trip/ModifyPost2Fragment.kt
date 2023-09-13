1489223571141c807b382470531f715679ba833d
JangYongJin <jjin9261@naver.com>
2023-09-13 14:41:56 +0900

    UI : 글 수정 달력 추가(#44_trip)
    달력추가

--- TripFriend/app/src/main/java/com/test/tripfriend/ui/trip/ModifyPost2Fragment.kt
+++ TripFriend/app/src/main/java/com/test/tripfriend/ui/trip/ModifyPost2Fragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.test.tripfriend.MainActivity
 import com.test.tripfriend.R
 import com.test.tripfriend.databinding.FragmentModifyPost2Binding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+import com.archit.calendardaterangepicker.customviews.CalendarListener
 
 class ModifyPost2Fragment : Fragment() {
     lateinit var fragmentModifyPost2Binding: FragmentModifyPost2Binding
@@ -30,6 +35,24 @@ class ModifyPost2Fragment : Fragment() {
             mainActivity.activityMainBinding.bottomNavigationViewMain.visibility = View.GONE
             buttonAccompanyModifyPost2ToNextView.setOnClickListener {
                 mainActivity.replaceFragment(MainActivity.MODFY_POST3_FRAGMENT,true,true,null)
+            }
+            textInputLayoutModifyPost2Date.run {
+                // 데이트 피커
+                fragmentModifyPost2Binding.calendarModifyPost2.setCalendarListener(object :
+                    CalendarListener {
+                    override fun onFirstDateSelected(startDate: Calendar) {
+                        val date = startDate.time
+                        val format = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+                        Toast.makeText(mainActivity, "Start Date: " + format.format(date), Toast.LENGTH_SHORT).show()
+                    }
+
+                    override fun onDateRangeSelected(startDate: Calendar, endDate: Calendar) {
+                        val startDate = startDate.time
+                        val endDate = endDate.time
+                        val format = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+                        Toast.makeText(mainActivity, "Start Date: " + format.format(startDate) + "\nEnd date: " + format.format(endDate), Toast.LENGTH_SHORT).show()
+                    }
+                })
             }
         }
 
