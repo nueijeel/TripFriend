322390440ecd280977a0b8f79457bbce93f16010
JangYongJin <jjin9261@naver.com>
2023-09-16 13:21:01 +0900

    feat : 로그인 임시 구현(#63_signup)

--- TripFriend/app/src/main/java/com/test/tripfriend/ui/user/EmailLoginFragment.kt
+++ TripFriend/app/src/main/java/com/test/tripfriend/ui/user/EmailLoginFragment.kt
@@ -7,10 +7,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
 import com.test.tripfriend.ui.main.MainActivity
 import com.test.tripfriend.R
 import com.test.tripfriend.databinding.FragmentEmailLoginBinding
+import com.test.tripfriend.repository.UserRepository
 
 class EmailLoginFragment : Fragment() {
 
@@ -39,13 +41,37 @@ class EmailLoginFragment : Fragment() {
 
             //로그인 버튼 클릭 이벤트
             buttonEmailLoginSubmit.setOnClickListener {
+                var checkMail = 0
+                var checkPw = 0
+                UserRepository.getAllUser {
+                    UserRepository.getAllUser() {
+                        for (document in it.result.documents) {
+                            if (textInputEditTextEmailLoginId.text.toString() == document.getString("userEmail")) {
+                                checkMail = 1
+                            }
+                            if(textInputEditTextEmailLoginPw.text.toString() == document.getString("userPw")) {
+                                checkPw = 1
+                            }
+                        }
+                        if(checkMail == 1 && checkPw == 1){
+                            Snackbar.make(fragmentEmailLoginBinding.root, "로그인이 완료되었습니다.", Snackbar.LENGTH_SHORT).show()
 
-                Snackbar.make(fragmentEmailLoginBinding.root, "로그인이 완료되었습니다.", Snackbar.LENGTH_SHORT).show()
+                            val intent = Intent(loginMainActivity, MainActivity::class.java)
+                            startActivity(intent)
 
-                val intent = Intent(loginMainActivity, MainActivity::class.java)
-                startActivity(intent)
+                            loginMainActivity.finish()
+                        }
+                        else{
+                            val builder= MaterialAlertDialogBuilder(loginMainActivity,R.style.DialogTheme).apply {
+                                setTitle("로그인 실패")
+                                setMessage("이메일과 비밀번호를 확인해주세요.")
+                                setNegativeButton("확인", null)
+                            }
+                            builder.show()
+                        }
+                    }
+                }
 
-                loginMainActivity.finish()
             }
         }
 
