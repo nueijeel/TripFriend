6310ca3881f8e4a0494cab34ef6452aedca1e8d5
Do-Re-Min-Woo <kimy5878@naver.com>
2023-09-18 17:37:19 +0900

    fix : tripPostIdx 추가

--- TripFriend/app/src/main/java/com/test/tripfriend/dataclassmodel/TripPost.kt
+++ TripFriend/app/src/main/java/com/test/tripfriend/dataclassmodel/TripPost.kt
@@ -1,17 +1,18 @@
 package com.test.tripfriend.dataclassmodel
 
-data class TripPost (
-    val tripPostWriterEmail : String,               //작성자이메일
-    val tripPostTitle : String,                     //제목
-    val tripPostMemberList : List<String>,          //참여자 이메일 리스트
-    val tripPostMemberCount : Int,                  //인원수
-    val tripPostImage : String,                     //사진
-    val tripPostDate : List<Int>,                   //날짜 idx 0-시작날짜, 1-마지막날짜
-    val tripPostLocationName : String,              //지역명
-    val tripPostLatitude : Double,                  //위도
-    val tripPostLongitude : Double,                 //경도
-    val tripPostLikedCount : Int,                   //찜 개수
-    val tripPostTripCategory : List<String>,        //여행 카테고리 (3개)
-    val tripPostHashTag : String,                   //해시태그
-    val tripPostContent : String                    //내용
+data class TripPost(
+    val tripPostWriterEmail: String? = null,               //작성자이메일
+    val tripPostTitle: String? = null,                     //제목
+    val tripPostMemberList: List<String>? = emptyList(),          //참여자 이메일 리스트
+    val tripPostMemberCount: Int = 0,                  //인원수
+    val tripPostImage: String? = null,                     //사진
+    val tripPostDate: List<String>? = emptyList(),                   //날짜 idx 0-시작날짜, 1-마지막날짜
+    val tripPostLocationName: String? = null,              //지역명
+    val tripPostLatitude: Double? = 0.0,                  //위도
+    val tripPostLongitude: Double? = 0.0,                 //경도
+    val tripPostLikedCount: Int = 0,                   //찜 개수
+    val tripPostTripCategory: List<String>? = emptyList(),        //여행 카테고리 (3개)
+    val tripPostHashTag: String? = null,                   //해시태그
+    val tripPostContent: String? = null,                   //내용
+    val tripPostIdx: Int = 0
 )
