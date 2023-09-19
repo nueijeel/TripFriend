c47e0ffd7b0bc0304195d622041c53444df1dd06
JIEUN\lge67 <lge6716@gmail.com>
2023-09-20 02:37:04 +0900

    fix : TripRequest 클래스 매개변수 기본값 설정

--- TripFriend/app/src/main/java/com/test/tripfriend/dataclassmodel/TripRequest.kt
+++ TripFriend/app/src/main/java/com/test/tripfriend/dataclassmodel/TripRequest.kt
@@ -1,9 +1,9 @@
 package com.test.tripfriend.dataclassmodel
 
 data class TripRequest(
-    val tripRequestPostId : String,             //동행글 id (document id)
-    val tripRequestWriterEmail : String,        //신청자 이메일
-    val tripRequestReceiverEmail : String,      //동행글 주인(요청 받는 사람) 이메일
-    val tripRequestCotent : String,             //자기소개 내용
-    val tripRequestState : String               //수락 상태
+    val tripRequestPostId : String = "",             //동행글 id (document id)
+    val tripRequestWriterEmail : String = "",        //신청자 이메일
+    val tripRequestReceiverEmail : String = "",      //동행글 주인(요청 받는 사람) 이메일
+    val tripRequestContent : String = "",             //자기소개 내용
+    val tripRequestState : String = ""               //수락 상태
 )
