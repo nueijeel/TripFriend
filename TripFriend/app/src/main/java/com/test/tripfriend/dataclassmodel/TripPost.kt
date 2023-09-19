e00293c503e3e84ccce5ac583842112f6e26f716
Ganghyungoo <104668071+Ganghyungoo@users.noreply.github.com>
2023-09-19 11:01:27 +0900

    feat: 단체 채팅 구현 완료(#83_inflate_my_group_chatiing_room)
    이미지와 나가기는 프리퍼런스 생기는대로 구현예정

--- TripFriend/app/src/main/java/com/test/tripfriend/dataclassmodel/TripPost.kt
+++ TripFriend/app/src/main/java/com/test/tripfriend/dataclassmodel/TripPost.kt
@@ -1,17 +1,18 @@
 package com.test.tripfriend.dataclassmodel
 
 data class TripPost (
-    val tripPostWriterEmail : String,               //작성자이메일
-    val tripPostTitle : String,                     //제목
-    val tripPostMemberList : List<String>,          //참여자 이메일 리스트
-    val tripPostMemberCount : Int,                  //인원수
-    val tripPostImage : String,                     //사진
-    val tripPostDate : List<Int>,                   //날짜 idx 0-시작날짜, 1-마지막날짜
-    val tripPostLocationName : String,              //지역명
-    val tripPostLatitude : Double,                  //위도
-    val tripPostLongitude : Double,                 //경도
-    val tripPostLikedCount : Int,                   //찜 개수
-    val tripPostTripCategory : List<String>,        //여행 카테고리 (3개)
-    val tripPostHashTag : String,                   //해시태그
-    val tripPostContent : String                    //내용
+    val tripPostWriterEmail : String?=null,               //작성자이메일
+    val tripPostTitle : String?=null,                     //제목
+    val tripPostMemberList : List<String>?=null,          //참여자 이메일 리스트
+    val tripPostMemberCount : Int?=null,                  //인원수
+    val tripPostImage : String?=null,                     //사진
+    val tripPostDate : List<Int>?=null,                   //날짜 idx 0-시작날짜, 1-마지막날짜//
+    val tripPostLocationName : String?=null,              //지역명
+    val tripPostLatitude : Double?=null,                  //위도
+    val tripPostLongitude : Double?=null,                 //경도
+    val tripPostLikedCount : Int?=null,                   //찜 개수
+    val tripPostTripCategory : List<String>?=null,        //여행 카테고리 (3개)
+    val tripPostHashTag : String?=null,                   //해시태그
+    val tripPostContent : String?=null,                    //내용//
+    val tripPostGender:String?=null
 )
