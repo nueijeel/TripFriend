49f0cffdf3f2d54b5f0bf4accdbb90ff6bd5cfcb
JIEUN\lge67 <lge6716@gmail.com>
2023-09-20 02:39:45 +0900

    feat : 받은 동행 요청 페이지 기능 구현
    요청 데이터 불러오기, 거절/수락 기능 구현
    ReceivedNotificationFragment에서 로그인 유저 정보 받아오는 처리와 요청 수락 시 채팅 멤버 데이터에 유저 추가하는 처리 추후 구현 예정

--- TripFriend/app/src/main/java/com/test/tripfriend/repository/TripPostRepository.kt
+++ TripFriend/app/src/main/java/com/test/tripfriend/repository/TripPostRepository.kt
@@ -1,6 +1,8 @@
 package com.test.tripfriend.repository
 
 import android.util.Log
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.QuerySnapshot
 import com.test.tripfriend.dataclassmodel.TripPost
@@ -15,5 +17,23 @@ class TripPostRepository {
         val docRef = db.collection("TripPost").get().await()
 
         return docRef
+    }
+
+    //해당하는 동행글 데이터만 가져오는 메서드
+    suspend fun getTargetUserTripPost(tripPostDocumentId : String) : DocumentSnapshot {
+        return db.collection("TripPost")
+            .document(tripPostDocumentId).get().await()
+    }
+
+    fun addTripMemberNickname(userNickname : String, tripPostDocumentId: String){
+        db.collection("TripPost")
+            .document(tripPostDocumentId)
+            .update("tripPostMemberList", FieldValue.arrayUnion(userNickname))
+            .addOnCompleteListener {
+                Log.d("addTripMember", "success")
+            }
+            .addOnFailureListener {
+                Log.d("addTripMember", "failure")
+            }
     }
 }
--- TripFriend/app/src/main/java/com/test/tripfriend/viewmodel/TripPostViewModel.kt
+++ TripFriend/app/src/main/java/com/test/tripfriend/viewmodel/TripPostViewModel.kt
@@ -1,16 +1,26 @@
 package com.test.tripfriend.viewmodel
 
+import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.ktx.toObject
 import com.test.tripfriend.dataclassmodel.TripPost
+import com.test.tripfriend.dataclassmodel.TripRequest
 import com.test.tripfriend.repository.TripPostRepository
 import kotlinx.coroutines.runBlocking
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class TripPostViewModel: ViewModel() {
     val tripPostRepository = TripPostRepository()
 
     val tripPostList = MutableLiveData<List<TripPost>>()
+
+    private val _tripPost = MutableLiveData<TripPost>()
+    val tripPost : LiveData<TripPost>
+        get() = _tripPost
 
     fun getTripPostData() {
         val currentTripPostSnapshot =
@@ -20,6 +30,28 @@ class TripPostViewModel: ViewModel() {
             val currentTripPost = currentTripPostSnapshot.toObjects(TripPost::class.java)
 
             tripPostList.value = currentTripPost
+        }
+    }
+
+    fun getTargetUserTripPost(tripPostDocumentId : String){
+        val tripPostDocumentSnapshot =
+            runBlocking {
+                tripPostRepository.getTargetUserTripPost(tripPostDocumentId)
+            }
+
+        if(tripPostDocumentSnapshot != null){
+            val currentTripPost = tripPostDocumentSnapshot.toObject(TripPost::class.java)
+
+            //현재 날짜 구하기
+            val currentDate = LocalDate.now()
+            val formatter = DateTimeFormatter.ofPattern("yyyyMMdd")
+            val date : Long = currentDate.format(formatter).toLong()
+
+            if(currentTripPost != null){
+                if(currentTripPost.tripPostDate!![0].toLong() > date){
+                    _tripPost.value = currentTripPost!!
+                }
+            }
         }
     }
 }
