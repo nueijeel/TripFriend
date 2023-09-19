9d0be3f14aee46ea1d3f0cafdd18127e46e1a5c6
JIEUN\lge67 <lge6716@gmail.com>
2023-09-20 02:35:48 +0900

    comment : 주석 수정

--- TripFriend/app/src/main/java/com/test/tripfriend/dataclassmodel/TripPost.kt
+++ TripFriend/app/src/main/java/com/test/tripfriend/dataclassmodel/TripPost.kt
@@ -3,7 +3,7 @@ package com.test.tripfriend.dataclassmodel
 data class TripPost(
     val tripPostWriterEmail: String = "",               //작성자이메일
     val tripPostTitle: String = "",                     //제목
-    val tripPostMemberList: List<String>? = emptyList(),          //참여자 이메일 리스트
+    val tripPostMemberList: List<String>? = emptyList(),          //참여자 닉네임 리스트
     val tripPostMemberCount: Int = 0,                  //인원수
     val tripPostImage: String? = "",                     //사진
     val tripPostDate: List<String>? = emptyList(),                   //날짜 idx 0-시작날짜, 1-마지막날짜
