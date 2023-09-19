26763390c167e58b527ee09a31dd00bbbbd9d76a
Do-Re-Min-Woo <kimy5878@naver.com>
2023-09-19 09:29:11 +0900

    feat : 탭바 리사이클러뷰 구현 중 (아직 문제가 있어요)

--- TripFriend/app/src/main/java/com/test/tripfriend/repository/TripPostRepository.kt
+++ TripFriend/app/src/main/java/com/test/tripfriend/repository/TripPostRepository.kt
@@ -1,22 +1,19 @@
 package com.test.tripfriend.repository
 
 import android.util.Log
-import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.QuerySnapshot
+import com.test.tripfriend.dataclassmodel.TripPost
+import kotlinx.coroutines.tasks.await
 
 class TripPostRepository {
+
     val db = FirebaseFirestore.getInstance()
 
     // 문서 가져오기 메서드
-    fun getDocumentData(): Task<QuerySnapshot> {
-        val docRef = db.collection("TripPost")
-            .get()
-            .addOnSuccessListener { result ->
-                for (document in result) {
-                    Log.d("ㅁㅇ", "${document.id} => ${document.data}")
-                }
-            }
+    suspend fun getDocumentData() : QuerySnapshot {
+        val docRef = db.collection("TripPost").get().await()
+
         return docRef
     }
 }
--- TripFriend/app/src/main/java/com/test/tripfriend/viewmodel/TripPostViewModel.kt
+++ TripFriend/app/src/main/java/com/test/tripfriend/viewmodel/TripPostViewModel.kt
@@ -1,11 +1,25 @@
 package com.test.tripfriend.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.DocumentSnapshot
+import com.test.tripfriend.dataclassmodel.TripPost
 import com.test.tripfriend.repository.TripPostRepository
+import kotlinx.coroutines.runBlocking
 
 class TripPostViewModel: ViewModel() {
     val tripPostRepository = TripPostRepository()
 
+    val tripPostList = MutableLiveData<List<TripPost>>()
+
     fun getTripPostData() {
+        val currentTripPostSnapshot =
+            runBlocking { tripPostRepository.getDocumentData() }
+
+        if(currentTripPostSnapshot != null) {
+            val currentTripPost = currentTripPostSnapshot.toObjects(TripPost::class.java)
+
+            tripPostList.value = currentTripPost
+        }
     }
 }
